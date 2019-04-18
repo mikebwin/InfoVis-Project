aad6760f20d0499af55f9ceda0fec0fe164cb3ad
mikebwin <mikebwin@gmail.com>
2019-04-18 18:19:04 -0400

    removed unavailable from first pie chart

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function start() {
 var graph = document.getElementById("graph")
 var fatality = document.getElementById("fatality")
 var severity = document.getElementById("severity")
+var phase = document.getElementById("phase")
 var bubble = document.getElementById("map")
 
 //width and height to define svgs
@@ -12,6 +13,8 @@ var width_fatality = 635
 var height_fatality = 600
 var width_severity = 600
 var height_severity = 600
+var width_phase = 600
+var height_phase = 600
 var width_map = 800
 var height_map = 600
 
@@ -25,13 +28,18 @@ var severity_graph = d3.select(severity)
     .attr("width", width_fatality)
     .attr("height", height_fatality)
 
+var phase_doughnut = d3.select(phase)
+    .append("svg")
+    .attr("width", width_phase)
+    .attr("height", height_phase)
+
 var bubble_map = d3.select(bubble)
     .append("svg")
     .attr("width", width_map)
     .attr("height", height_map)
 
 //FATALITIES STUFF
-
+{
 var bars = fatality_graph.attr("id", "fatalities_by_year");
 var xScaleFatality = d3.scaleLinear().range([45, width_fatality-40])
 var yScaleFatality = d3.scaleLinear().range([height_fatality - 50, 0])
@@ -94,35 +102,29 @@ function changefreq() {
     }
     freq_threshold = selectValue
 }
+}
 
 //SEVERITY STUFF
 var pie_chart = severity_graph.append('g').attr("id", "severity_breakdown")
-
 var radius_severity = (Math.min(height_severity, width_severity) - 30)/ 2
-var color = d3.scaleOrdinal().range(["#363847","#aeb2bf","#e1575c", "#818a8b"])
+
+var color = d3.scaleOrdinal().range(["#ffcaca", "#beb1f1", "#c5e6f8", "#fff89c", "#febca9", "#c69afa", "#c5b9cd","#a4ae9e","#363847","#aeb2bf","#e1575c", "#818a8b", "#b4cae1", "#d0adaf", "#403f69"])
+
+//PHASE DOUGHNUT
+var phase_chart = phase_doughnut.append('g').attr("id", "phase_breakdown")
+var radius_phase = (Math.min(height_phase, width_phase) - 30)/2
 
 //MAP STUFF
-// var map = bubble_map.append("g")
+var projection = d3.geoStereographic()
+    .scale(width_map / 2 / Math.PI)
+    .scale(200)
+    .translate([width_map / 2, height_map / 2])
 
-// var projection = d3.geo.albersUsa()
-//     .scale(1070)
-//     .translate([width_map / 2, height_map / 2]);
+var path = d3.geoPath()
+    .projection(projection);
 
-// var path = d3.geo.path()
-//     .projection(projection);
+var url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson";
 
-// d3.json("us.json", function(error, us){
-//     if (error) throw error;
-//     map.append("g")
-//         .attr("id", "states")
-//         .selectAll("path")
-//         .data(topojson.feature(us, us.objects.states).features)
-//         .enter().append("path")
-//         .attr("d", path)
-//         .on("click", function(d){
-
-//         })
-// })
 
 //START DATA AND D3
 d3.csv("aircraft_incidents.csv", function(csv){
@@ -139,6 +141,7 @@ d3.csv("aircraft_incidents.csv", function(csv){
         csv[i].carrier = csv[i].Air_Carrier.toUpperCase()
         csv[i].phase = csv[i].Broad_Phase_of_Flight.toUpperCase()
         csv[i].fatalities = csv[i].Total_Fatal_Injuries
+        csv[i].coordinates = [csv[i].Longitude, csv[i].Latitude]
     }
 
     //START FATALITIES BY YEAR
@@ -242,9 +245,7 @@ d3.csv("aircraft_incidents.csv", function(csv){
     }
 
     
-
     // START LOCATION OF PERCENTAGE OF INJURY SEVERITY
-    
     {
 
     var breakdown = d3.nest().key(function(d) {
@@ -253,12 +254,17 @@ d3.csv("aircraft_incidents.csv", function(csv){
         return v.length
     }).entries(csv)
 
-    console.log(breakdown)
+    var new_break = breakdown.filter(function(d) {
+            if (d.key != "Unavailable") {
+                console.log(d.key)
+                return d.key
+            }
+    })
 
     var pie = d3.pie().value(function(d){
         return d.value;
-    })(breakdown);   
-
+    })(new_break);  
+    
     var arc = d3.arc()
         .outerRadius(radius_severity - 10)
         .innerRadius(width_severity/5);
@@ -276,29 +282,112 @@ d3.csv("aircraft_incidents.csv", function(csv){
         .attr("d", arc)
         .style("fill", function(d) { return color(d.data.key)})
         .on("mouseover", function(d){
-            arcs.select("text").text(d.data.key + "\n" + d.data.value)
+            arcs.select("text").text(d.data.key + ": " + d.data.value)
             //for some reason color doesn't work. I'm frustrated
-            .style("fill", function(v) { return color(v.data.key)})
+            .style("fill", function(v) { return "black"})
             .attr("stroke", "0")
         })
         
     arcs.append("text")
         .attr("id", "pie-inner-text")
         .attr("text-anchor", "middle")
-        .attr("font-size", "30px")
+        .attr("font-size", "25px")
         .attr("y", 20)
+        .attr("color", "black")
         
     }
-    
-    // START MAP
-    bubble_map.append("rect")
-        .attr("class", "background")
-        .attr("width", width_map)
-        .attr("height", height_map)
-        .on("click", function(d){
 
+    //ANOTHER DOUGHNUT
+    {
+
+    var phase_breakdown = d3.nest().key(function(d) {
+    return d.phase
+    }).rollup(function(v){
+    return v.length
+    }).entries(csv)
+
+    var new_phase_breakdown = phase_breakdown.filter(function(d){
+        return d.key.length > 0
+    });
+
+    phase_pie_chart = d3.pie().value(function(d){
+    return d.value;
+    })(new_phase_breakdown);   
+
+    var phase_arc = d3.arc()
+    .outerRadius(radius_phase - 10)
+    .innerRadius(width_phase/5);
+    var phase_label = d3.arc()
+    .outerRadius(radius_phase - 40)
+    .innerRadius(radius_phase - 40)
+
+    phase_chart.attr("transform", "translate(" + width_phase/2 + "," + height_severity/2 + ")")
+    var phase_arcs = phase_chart.selectAll("arc")
+    .data(phase_pie_chart)
+    .enter()
+    .append("g")
+    .attr("class", "arc")
+    phase_arcs.append("path")
+    .attr("d", phase_arc)
+    .style("fill", function(d) { return color(d.data.key)})
+    .on("mouseover", function(d){
+        phase_arcs.select("text").text(d.data.key + ": " + d.data.value)
+        //for some reason color doesn't work. I'm frustrated
+        .style("fill", function(v) { return "black"})
+        .attr("stroke", "0")
+    })
+    
+    phase_arcs.append("text")
+    .attr("id", "phase-doughtnut")
+    .attr("text-anchor", "middle")
+    .attr("font-size", "25px")
+    .attr("y", 20)
+    .attr("color", "black")
+        
+    }
+
+    // START MAP
+    d3.json(url, function(error, world_geojson) {
+
+        console.log(world_geojson)
+
+        var coordinates_breakdown = d3.nest().key(function(d){
+            return [Math.round(d.coordinates[0]*2)/2, Math.round(d.coordinates[1]*2)/2]
+        }).rollup(function(v){
+            return v.length
+        }).entries(csv)
+
+        var coordinates = coordinates_breakdown.filter(function(d){
+            return d.key.length > 3
         })
 
+        // console.log(world_geojson.features.type)
+
+        // bubble_map.append("g")
+        //     .attr("id", "countries")
+        //     .selectAll("path")
+        //     .data(topojson.feature(world_geojson, world_geojson.features).features)
+
+        bubble_map.append("path")
+            .attr("d", path(world_geojson)).attr("fill", "lightgray")
+        
+        bubble_map.selectAll("circle")
+            .data(coordinates)
+            .enter()
+            .append("circle")
+            .attr("r", function(d) {
+                return d.value
+            })
+            .attr("cx", function(d){
+                arr = d.key.split(",")
+                return projection(arr)[0]
+            })
+            .attr("cy", function(d){
+                arr = d.key.split(",")
+                return projection(arr)[1]
+            })
+            .attr("opacity", .4)
+    })
     
 })
 
